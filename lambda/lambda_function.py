c4d518a6e17cd69cf453f19fbc842d4cee989d0d
zelkin2 <brandi_lon@hotmail.com>
2024-12-02 02:01:07 +0000

    initial setup for lambda and github actions

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,6 +3,6 @@ import json
 def lambda_hamdler(event, context):
     return {
      'StatusCode': 200,
-     'body': json.dumps('Hello from CICD Lambda')     
+     'body': json.dumps('Hello from CICD github actions worklow vs code')     
        
     } 
